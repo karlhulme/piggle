93f84127dc476c52876373e62805229b203fcd61
Karl Hulme <karlhulme@hotmail.com>
2020-12-30 10:19:33 +0000

    --fix Added Object.setPrototypeOf call to the errors.

--- src/errors/SaveCallbackError.ts
+++ src/errors/SaveCallbackError.ts
@@ -9,6 +9,7 @@ export class SaveCallbackError extends Error {
    */
   constructor (message: string) {
     super(message)
+    Object.setPrototypeOf(this, new.target.prototype)
     this.name = this.constructor.name
   }
 }
